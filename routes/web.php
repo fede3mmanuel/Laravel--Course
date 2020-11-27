0431347918046b1721d9c12ead5e0b6f449bda18
fede <fede@fede.website>
2020-11-27 17:28:01 -0600

    Casi finalizado

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,12 @@ Route::get('/', HomeController::class);
 
 //Route::get('/', 'HomeController');
 
-Route::get('cursos', [CursoController::class, 'index']);
+Route::get('cursos', [CursoController::class, 'index'])->name('cursos.index');
 //Route::get('cursos', 'CursoController@index');
 
-Route::get('cursos/create', [CursoController::class, 'create']);
+Route::get('cursos/create', [CursoController::class, 'create'])->name('cursos.create');
 
-Route::get('cursos/{curso}', [CursoController::class, 'show']);
+Route::get('cursos/{id}', [CursoController::class, 'show'])->name('cursos.show');
 /* 
 Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
 
